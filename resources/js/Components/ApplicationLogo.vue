ab4e4117fd7971a2771473e99022177eb8aabc5d
Kamil Narygin <Saybayry@yandex.ru>
2024-12-02 02:40:30 +0700

    initial commit

---
+++ resources/js/Components/ApplicationLogo.vue
@@ -0,0 +1,68 @@
+<template>
+<svg
+   width="45.973236mm"
+   height="56.860504mm"
+   viewBox="0 0 45.973236 56.860504"
+   version="1.1"
+   class="w-12 h-12 fill-current stroke-current text-indigo-600 dark:text-white dark:stroke-white"
+
+   id="svg5"
+   xml:space="preserve"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+   <sodipodi:namedview
+     id="namedview7"
+     pagecolor="#505050"
+     bordercolor="#eeeeee"
+     borderopacity="1"
+     inkscape:showpageshadow="0"
+     inkscape:pageopacity="0"
+     inkscape:pagecheckerboard="0"
+     inkscape:deskcolor="#505050"
+     inkscape:document-units="mm"
+     showgrid="false" /><defs
+     id="defs2"><rect
+       x="373.38336"
+       y="245.08572"
+       width="124.57396"
+       height="90.045303"
+       id="rect1672" /></defs><g
+     inkscape:label="Слой 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(-106.0997,-46.977072)">
+     <g id="g2661"
+       transform="translate(20.600121,0.35826297)">
+       <g id="g1373">
+        <a
+           id="a1010"
+           style="stroke:currentColor;stroke-opacity:1"><path
+             id="rect300"
+             style="fill:#bd0000;fill-opacity:0;stroke:currentColor;stroke-width:3.1773;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:1"
+             d="m 96.990053,48.207458 h 28.950477 c 2.1845,0 3.94314,1.75864 3.94314,3.943138 v 47.570241 c 0,1.995453 0.0147,2.169823 -2.16983,2.169823 H 97.496713 c -5.47779,0 -10.296046,-3.53244 -10.276391,-9.263068 l 0.126665,-36.930374 c 0.01792,-5.225555 5.811922,-7.48976 9.643066,-7.48976 z"
+             sodipodi:nodetypes="sssssssss" /></a><path
+           style="fill:#bd0000;fill-opacity:0;stroke:currentColor;stroke-width:3.292;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1"
+           d="m 102.59872,77.582364 h 14.18649"
+           id="path1365" />
+           <path
+           style="fill:#bd0000;fill-opacity:0;stroke:currentColor;stroke-width:3.292;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1"
+           d="M 128.69173,84.485611 H 95.758806 c 0,0 -8.359897,0.379997 -8.613227,8.359897"
+           id="path1367" /><g
+           aria-label="АГУ"
+           transform="matrix(0.26458333,0,0,0.26458333,-2.7049759,-5.9810368)"
+           id="text1670"
+           style="font-size:53.3333px;font-family:Alef;-inkscape-font-specification:Alef;white-space:pre; fill-opacity:0;stroke:currentColor;stroke-width:7.15087;stroke-linecap:round;stroke-linejoin:round"><path
+             d="m 409.16404,293.74353 h -5.49479 l -3.80208,-10.80729 h -16.77082 l -3.80208,10.80729 h -5.23437 l 14.11457,-38.77602 h 6.875 z m -10.88541,-15.23437 -6.79687,-19.03645 -6.82291,19.03645 z"
+             id="path1919" />
+             <path
+             d="M 440.10152,259.55084 H 420.2057 v 34.19269 h -5.15624 v -38.77602 h 25.05206 z"
+             id="path1921" />
+             <path
+             d="m 472.70567,254.96751 -14.73958,32.63019 q -0.88541,1.97916 -2.10937,3.2552 -1.19792,1.27604 -2.60416,1.97917 -1.35417,0.65104 -2.63021,0.88541 -1.25,0.20834 -2.42187,0.20834 -1.04167,0 -2.52604,-0.10417 -1.48438,-0.10417 -1.90105,-0.13021 v -4.8177 h 0.36459 q 0.39062,0.0521 1.30208,0.18229 0.91146,0.10416 2.08333,0.10416 0.52084,0 1.45833,-0.0781 0.9375,-0.0781 1.66667,-0.36458 0.83333,-0.3125 1.64062,-0.98959 0.83334,-0.67708 1.40625,-1.97916 l 0.9375,-2.05729 -14.42707,-28.72394 h 5.70312 l 11.32812,22.86457 10.15624,-22.86457 z"
+             id="path1923" />
+            </g></g></g></g>
+</svg>
+
+</template>
---
+++ resources/js/Components/StudentGroupTable.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref } from 'vue';
+import StudentTable from '@/Components/StudentTable.vue';
+// Управление состоянием
+const activeIndex = ref(null); // Индекс активного элемента
+const items = ref([
+  {
+    title: '205s11-5',
+    content: 'Details for Invoice 1: Amount $20,000, Issued on Sept 28, 2019, Due in 3 days.',
+  },
+  {
+    title: '205s11-4',
+    content: 'Details for Invoice 2: Amount $214,000, Issued on Sept 25, 2019, Due in 6 days.',
+  },
+  {
+    title: '205s11-3',
+    content: 'Details for Invoice 3: Amount $10,000, Issued on Oct 5, 2019, Due in 2 weeks.',
+  },
+]);
+
+// Метод для переключения
+const toggleItem = (index) => {
+  activeIndex.value = activeIndex.value === index ? null : index;
+};
+</script>
+
+<template>
+  <div class="container mx-auto px-4 sm:px-8 py-8">
+    <h2 class="text-2xl font-semibold leading-tight mb-4">Accordion</h2>
+
+    <div class="shadow-md rounded-lg border border-gray-200">
+      <!-- Аккордеон, начинаем итерацию -->
+      <div v-for="(item, index) in items" :key="index" class="border-b last:border-b-0">
+        <!-- Заголовок аккордеона -->
+        <button
+          @click="toggleItem(index)"
+          class="w-full flex justify-between items-center px-6 py-4 bg-gray-100 text-left text-gray-700 hover:bg-gray-200 focus:outline-none"
+        >
+          <span class="font-medium">{{ item.title }}</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 transition-transform duration-300"
+            :class="{ 'rotate-180': activeIndex === index }"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+          </svg>
+        </button>
+
+        <!-- Контент аккордеона -->
+        <div
+          v-if="activeIndex === index"
+          class="px-6 py-4 bg-white text-gray-700"
+        >
+          <StudentTable></StudentTable>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
